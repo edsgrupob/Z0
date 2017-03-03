10696cc869586cb6e4e2d0eef11850b87261ff02
BrunaKimura <brunamk@al.insper.edu.br>
2017-03-02 22:26:37 -0300

    nor8way primeira tentativa

--- Computador/Modulos/PortasLogicas/Nor8Way.vhd
+++ Computador/Modulos/PortasLogicas/Nor8Way.vhd
@@ -13,3 +13,18 @@ entity Nor8Way is
 			h:   in  STD_LOGIC;
 			q:   out STD_LOGIC);
 end entity;
+
+architecture arch_nor8way of Nor8Way is
+component chip_nor
+end component;
+
+signal w1,w2,w3,w4,w5,w6: STD_LOGIC;
+begin
+	w1 <= a nor b;
+	w2 <= c nor d;
+	w3 <= e nor f;
+	w4 <= g nor h;
+	w5 <= w1 nor w2;
+	w6 <= w3 nor w4; 
+	q <= w5 nor w6;
+end architecture;
