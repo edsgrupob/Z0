cc6d3c5bbc04951a35106bad9c45b46d7c6eeec7
Raphael costa <Raphael costa>
2017-03-06 23:20:40 -0300

    Portas Mux16, Mux8Way, Or16 e Or8Way

--- Computador/Modulos/PortasLogicas/Or8Way.vhd
+++ Computador/Modulos/PortasLogicas/Or8Way.vhd
@@ -13,3 +13,15 @@ entity Or8Way is
 			h:   in  STD_LOGIC;
 			q:   out STD_LOGIC);
 end entity;
+
+architecture arch_Or8Way of Or8Way is 
+signal w1,w2,w3,w4,w5,w6: STD_LOGIC;
+begin
+	w1 <= a or b;
+	w2 <= c or d;
+	w3 <= e or f;
+	w4 <= g or h;
+	w5 <= w1 or w2;
+	w6 <= w3 or w4;
+	q <= w5 or w6;
+end architecture;
