c6b8144bb43933fcdec2b0081ed25ed4a8e88e98
Filipe Foschini Borba <filipefborba@gmail.com>
2017-03-07 19:16:18 -0300

    FullAdder e And16_fix

--- Computador/Modulos/UnidadeLogicaAritmetica/FullAdder.vhd
+++ Computador/Modulos/UnidadeLogicaAritmetica/FullAdder.vhd
@@ -10,8 +10,8 @@ entity FullAdder is
 	);
 end entity;
 architecture arch_FullAdder of FullAdder is 
-begin
 signal a_b_xor, a_b_and, abxor_c_and : STD_LOGIC;
+begin
 	a_b_xor <= a xor b;
 	a_b_and <= a and b;
 	abxor_c_and <= a_b_xor and c;
