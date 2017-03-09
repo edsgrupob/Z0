f1977c5afa08605a612f232e46e20f167678227d
BrunaKimura <brunamk@al.insper.edu.br>
2017-03-09 19:05:53 -0300

    mux tava dando errado

--- Computador/Modulos/PortasLogicas/Mux4Way16.vhd
+++ Computador/Modulos/PortasLogicas/Mux4Way16.vhd
@@ -12,17 +12,9 @@ entity Mux4Way16 is
 end entity;
 
 architecture ChipMux4Way16 of Mux4way16 is
-
-signal not_s0, not_s1 : STD_LOGIC;
-
 begin
-
-	not_s0 <= not sel(0);
-	not_s1 <= not sel(1);
-	
-	q <= a and not_s0 and not_s1 else
-	b and sel(0) and not_s1 else
-	c and not_s0 and sel(1) else
-	d and sel(0) and sel(1) else a;
-	
-end ;
+	q <= a when (sel = "00") else
+		  b when (sel = "01") else
+		  c when (sel = "10") else
+		  d when (sel ="11") else a;
+end;
