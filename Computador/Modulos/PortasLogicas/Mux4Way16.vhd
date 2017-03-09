668ef98b025ffa5fd778853a3d62d95c39e74819
Filipe Foschini Borba <filipefborba@gmail.com>
2017-03-09 18:27:55 -0300

    Quick commit

--- Computador/Modulos/PortasLogicas/Mux4Way16.vhd
+++ Computador/Modulos/PortasLogicas/Mux4Way16.vhd
@@ -11,3 +11,18 @@ entity Mux4Way16 is
 			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
 
+architecture ChipMux4Way16 of Mux4way16 is
+
+signal not_s0, not_s1 : STD_LOGIC;
+
+begin
+
+	not_s0 <= not sel(0);
+	not_s1 <= not sel(1);
+	
+	q <= a and not_s0 and not_s1 else
+	b and sel(0) and not_s1 else
+	c and not_s0 and sel(1) else
+	d and sel(0) and sel(1) else a;
+	
+end ;
