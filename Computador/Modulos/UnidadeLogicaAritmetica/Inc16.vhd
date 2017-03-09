5c73ff4663b29520bde7dbb8fe04f7d57d9cef3d
Yiaannn <Yiaannn@gmail.com>
2017-03-09 18:46:45 -0300

    push de Inc16

--- Computador/Modulos/UnidadeLogicaAritmetica/Inc16.vhd
+++ Computador/Modulos/UnidadeLogicaAritmetica/Inc16.vhd
@@ -9,4 +9,36 @@ entity Inc16 is
 		a   :  in STD_LOGIC_VECTOR(15 downto 0);
 		q   : out STD_LOGIC_VECTOR(15 downto 0) 
 	);
-end entity; 
+end entity;
+
+entity HalfAdder is
+	port(
+		a,b:         in STD_LOGIC;   -- entradas
+		soma,vaium: out STD_LOGIC   -- sum e carry
+	);
+end entity;
+
+architecture arch_Inc16 of Inc16 is 
+signal carry : STD_LOGIC;
+begin
+
+    hold: HalfAdder port map (a(0),'1',a(0),carry);
+    hold: HalfAdder port map (a(1),carry,a(1),carry);
+    hold: HalfAdder port map (a(2),carry,a(2),carry);
+    hold: HalfAdder port map (a(3),carry,a(3),carry);
+    
+    hold: HalfAdder port map (a(4),carry,a(4),carry);
+    hold: HalfAdder port map (a(5),carry,a(5),carry);
+    hold: HalfAdder port map (a(6),carry,a(6),carry);
+    hold: HalfAdder port map (a(7),carry,a(7),carry);
+    
+    hold: HalfAdder port map (a(8),carry,a(8),carry);
+    hold: HalfAdder port map (a(9),carry,a(9),carry);
+    hold: HalfAdder port map (a(10),carry,a(10),carry);
+    hold: HalfAdder port map (a(11),carry,a(11),carry);
+    
+    hold: HalfAdder port map (a(12),carry,a(12),carry);
+    hold: HalfAdder port map (a(13),carry,a(13),carry);
+    hold: HalfAdder port map (a(14),carry,a(14),carry);
+    hold: HalfAdder port map (a(15),carry,a(15),carry);
+end architecture;
