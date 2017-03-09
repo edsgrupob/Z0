7c86460a0b69ffafb131867f904897c6b28fb3fb
Yiaannn <Yiaannn@gmail.com>
2017-03-09 18:59:08 -0300

    Modificado Inc16

--- Computador/Modulos/UnidadeLogicaAritmetica/Inc16.vhd
+++ Computador/Modulos/UnidadeLogicaAritmetica/Inc16.vhd
@@ -22,23 +22,23 @@ architecture arch_Inc16 of Inc16 is
 signal carry : STD_LOGIC;
 begin
 
-    hold: HalfAdder port map (a(0),'1',a(0),carry);
-    hold: HalfAdder port map (a(1),carry,a(1),carry);
-    hold: HalfAdder port map (a(2),carry,a(2),carry);
-    hold: HalfAdder port map (a(3),carry,a(3),carry);
+    h0: HalfAdder port map (a(0),'1',a(0),carry);
+    h1: HalfAdder port map (a(1),carry,a(1),carry);
+    h2: HalfAdder port map (a(2),carry,a(2),carry);
+    h3: HalfAdder port map (a(3),carry,a(3),carry);
     
-    hold: HalfAdder port map (a(4),carry,a(4),carry);
-    hold: HalfAdder port map (a(5),carry,a(5),carry);
-    hold: HalfAdder port map (a(6),carry,a(6),carry);
-    hold: HalfAdder port map (a(7),carry,a(7),carry);
+    h4: HalfAdder port map (a(4),carry,a(4),carry);
+    h5: HalfAdder port map (a(5),carry,a(5),carry);
+    h6: HalfAdder port map (a(6),carry,a(6),carry);
+    h7: HalfAdder port map (a(7),carry,a(7),carry);
     
-    hold: HalfAdder port map (a(8),carry,a(8),carry);
-    hold: HalfAdder port map (a(9),carry,a(9),carry);
-    hold: HalfAdder port map (a(10),carry,a(10),carry);
-    hold: HalfAdder port map (a(11),carry,a(11),carry);
+    h8: HalfAdder port map (a(8),carry,a(8),carry);
+    h9: HalfAdder port map (a(9),carry,a(9),carry);
+    h10: HalfAdder port map (a(10),carry,a(10),carry);
+    h11: HalfAdder port map (a(11),carry,a(11),carry);
     
-    hold: HalfAdder port map (a(12),carry,a(12),carry);
-    hold: HalfAdder port map (a(13),carry,a(13),carry);
-    hold: HalfAdder port map (a(14),carry,a(14),carry);
-    hold: HalfAdder port map (a(15),carry,a(15),carry);
+    h12: HalfAdder port map (a(12),carry,a(12),carry);
+    h13: HalfAdder port map (a(13),carry,a(13),carry);
+    h14: HalfAdder port map (a(14),carry,a(14),carry);
+    h15: HalfAdder port map (a(15),carry,a(15),carry);
 end architecture;
