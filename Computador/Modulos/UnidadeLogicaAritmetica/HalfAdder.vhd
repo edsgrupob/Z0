388fcc8c50aaa8025cb15b7917d3620e4dbaaccc
raphacosta27 <rapha2706@hotmail.com>
2017-03-09 18:19:26 -0300

    Porta Half Adder

--- Computador/Modulos/UnidadeLogicaAritmetica/HalfAdder.vhd
+++ Computador/Modulos/UnidadeLogicaAritmetica/HalfAdder.vhd
@@ -1,5 +1,5 @@
 -- Half Adder
-
+-- Half Adder contem: sum é um xor e carry é um and
 Library ieee;
 use ieee.std_logic_1164.all;
 
@@ -9,3 +9,8 @@ entity HalfAdder is
 		soma,vaium: out STD_LOGIC   -- sum e carry
 	);
 end entity;
+architecture chip_HalfAdder of HalfAdder is
+begin
+	soma <= a xor b;
+	vaium <= a and b;
+end architecture;
