e52f14ea7cbabff43ffc0b4997288e15f73bd1c5
chends888 <lchends@gmail.com>
2017-03-09 22:05:37 -0300

    SHIFTERS FUFANDO

--- Computador/Modulos/PortasLogicas/BarrelShifter16.vhd
+++ Computador/Modulos/PortasLogicas/BarrelShifter16.vhd
@@ -8,3 +8,47 @@ entity BarrelShifter16 is
 			size: in  std_logic_vector(2 downto 0);    -- shift amount
 			q:    out STD_LOGIC_VECTOR(15 downto 0));  -- output vector (shifted)
 end entity;
+architecture chip_BarrelShifter16 of BarrelShifter16 is
+begin
+	process (a, size)
+		begin
+		if (dir = '1') then
+			if (size ="000") then
+				q <= a;
+			elsif (size = "001") then
+				q <= a(0) & a (15 downto 1);
+			elsif (size ="010") then
+				q <= a(1 downto 0) & a(15 downto 2);
+			elsif (size ="011") then
+				q <= a(2 downto 0) & a(15 downto 3);
+			elsif (size ="100") then
+				q <= a(3 downto 0) & a(15 downto 4);
+			elsif (size ="101") then
+				q <= a(4 downto 0) & a(15 downto 5);
+			elsif (size ="110") then
+				q <= a(5 downto 0) & a(15 downto 6);
+			elsif (size ="111") then
+				q <= a(6 downto 0) & a(15 downto 7);
+			end if;
+		end if;
+		if (dir = '0') then
+			if (size="000") then
+				q <= a;
+			elsif (size ="001") then
+				q <= a(14 downto 0) & a(15);
+			elsif (size ="010") then
+				q <= a(13 downto 0) & a(15 downto 14);
+			elsif (size ="011") then
+				q <= a(12 downto 0) & a(15 downto 13);
+			elsif (size ="100") then
+				q <= a(11 downto 0) & a(15 downto 12);
+			elsif (size ="101") then
+				q <= a(10 downto 0) & a(15 downto 11);
+			elsif (size ="110") then
+				q <= a(9 downto 0) & a(15 downto 10);
+			elsif (size ="111") then
+				q <= a(8 downto 0) & a(15 downto 9);
+			end if;
+		end if;
+	end process;
+end architecture;
--- Computador/Modulos/PortasLogicas/BarrelShifter8.vhd
+++ Computador/Modulos/PortasLogicas/BarrelShifter8.vhd
@@ -8,3 +8,47 @@ entity BarrelShifter8 is
 			size: in  std_logic_vector(2 downto 0);    -- shift amount
 			q:    out STD_LOGIC_VECTOR(7 downto 0));  -- output vector (shifted)
 end entity;
+architecture chip_BarrelShifter8 of BarrelShifter8 is
+begin
+	process (a, size)
+		begin
+		if (dir = '1') then
+			if (size ="000") then
+				q <= a;
+			elsif (size = "001") then
+				q <= a(0) & a (7 downto 1);
+			elsif (size ="010") then
+				q <= a(1 downto 0) & a(7 downto 2);
+			elsif (size ="011") then
+				q <= a(2 downto 0) & a(7 downto 3);
+			elsif (size ="100") then
+				q <= a(3 downto 0) & a(7 downto 4);
+			elsif (size ="101") then
+				q <= a(4 downto 0) & a(7 downto 5);
+			elsif (size ="110") then
+				q <= a(5 downto 0) & a(7 downto 6);
+			elsif (size ="111") then
+				q <= a(6 downto 0) & a(7);
+			end if;
+		end if;
+		if (dir = '0') then
+			if (size="000") then
+				q <= a;
+			elsif (size ="001") then
+				q <= a(6 downto 0) & a(7);
+			elsif (size ="010") then
+				q <= a(5 downto 0) & a(7 downto 6);
+			elsif (size ="011") then
+				q <= a(4 downto 0) & a(7 downto 5);
+			elsif (size ="100") then
+				q <= a(3 downto 0) & a(7 downto 4);
+			elsif (size ="101") then
+				q <= a(2 downto 0) & a(7 downto 3);
+			elsif (size ="110") then
+				q <= a(1 downto 0) & a(7 downto 2);
+			elsif (size ="111") then
+				q <= a(0) & a(7 downto 1);
+			end if;
+		end if;
+	end process;
+end architecture;
