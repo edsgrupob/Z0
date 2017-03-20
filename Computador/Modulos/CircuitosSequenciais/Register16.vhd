452c58aa786f5a8922610f0a59ba94328146211a
Vitória Helena <vitoriahmc@al.insper.edu.br>
2017-03-20 11:20:02 -0300

    testando 16bit

--- Computador/Modulos/CircuitosSequenciais/Register16.vhd
+++ Computador/Modulos/CircuitosSequenciais/Register16.vhd
@@ -5,6 +5,17 @@
 Library ieee; 
 use ieee.std_logic_1164.all;
   
+entity Register8 is
+	port(
+		clock:   in STD_LOGIC;
+		input:   in STD_LOGIC_VECTOR(7 downto 0);
+		load:    in STD_LOGIC;
+		output: out STD_LOGIC_VECTOR(7 downto 0)
+	);
+end entity;Library ieee;
+ 
+use ieee.std_logic_1164.all;
+  
 entity Register16 is
 	port(
 		clock:   in STD_LOGIC;
@@ -13,3 +24,16 @@ entity Register16 is
 		output: out STD_LOGIC_VECTOR(15 downto 0)
 	);
 end entity;
+architecture chip_Register16 of Register16 is
+component Register8 is
+	port(
+		clock:   in STD_LOGIC;
+		input:   in STD_LOGIC_VECTOR(7 downto 0);
+		load:    in STD_LOGIC;
+		output: out STD_LOGIC_VECTOR(7 downto 0)
+	);
+end component;
+begin
+	u1: Register8 port map(clock, input(7 downto 0), load, output(7 downto 0));
+	u2: Register8 port map(clock, input(15 downto 8), load, output(15 downto 8));
+end architecture;
