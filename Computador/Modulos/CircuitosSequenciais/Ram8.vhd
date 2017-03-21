86dbd66b9c22f8b527a7306279ec196930320ea0
Vitória Helena <vitoriahmc@al.insper.edu.br>
2017-03-21 17:07:20 -0300

    ram8

--- Computador/Modulos/CircuitosSequenciais/Ram8.vhd
+++ Computador/Modulos/CircuitosSequenciais/Ram8.vhd
@@ -4,7 +4,7 @@
 
 Library ieee; 
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
   
 entity Ram8 is
 	port(
@@ -18,18 +18,17 @@ end entity;
 
 architecture arch_Ram8 of Ram8 is
    type memory is array(0 to 7) of std_logic_vector(15 downto 0);
-   signal ram_block : memory;
+   signal signal_ram : memory;
 
 begin
 
 	process(clock)
 	begin
-		if (clock'event and clock='1') then
-			if (load='1') then
-				ram_block(to_integer(unsigned(address))) <= input;
+		if (clock'event and clock='1' and load='1') then
+				signal_ram(conv_integer(address)) <= input;
 			end if;
 		end if;
 	end process;
-	output <= ram_block(to_integer(unsigned(address)));
+	output <= signal_ram(conv_integer(address));
 
 end architecture;
