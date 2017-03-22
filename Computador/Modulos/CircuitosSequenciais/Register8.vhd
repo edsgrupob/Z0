52e34f36893f1c2a291e8b10bce77f4efbcdb2be
IsabellaOInsper <isabellaro@al.insper.edu.br>
2017-03-21 23:38:15 -0300

    Tentativa 1 de 8bit

--- Computador/Modulos/CircuitosSequenciais/Register8.vhd
+++ Computador/Modulos/CircuitosSequenciais/Register8.vhd
@@ -13,3 +13,25 @@ entity Register8 is
 		output: out STD_LOGIC_VECTOR(7 downto 0)
 	);
 end entity;
+
+architecture arch_Register8 of Register8 is
+
+component BinaryDigit
+	port(
+		clock:   in STD_LOGIC;
+		input:   in STD_LOGIC;
+		load:    in STD_LOGIC;
+		output: out STD_LOGIC
+	);
+end component;
+
+begin 
+	u1: BinaryDigit port map (input[0], clock, output[0], load);
+	u2: BinaryDigit port map (input[1], clock, output[1], load);
+	u3: BinaryDigit port map (input[2], clock, output[2], load);
+	u4: BinaryDigit port map (input[3], clock, output[3], load);
+	u5: BinaryDigit port map (input[4], clock, output[4], load);
+	u6: BinaryDigit port map (input[5], clock, output[5], load);
+	u7: BinaryDigit port map (input[6], clock, output[6], load);
+	u8: BinaryDigit port map (input[7], clock, output[7], load);
+end architecture;
