38c4bba91e0f5a9e5fc38b083a44f647d3b095d7
Otofuji <ericfoa@al.insper.edu.br>
2017-03-21 21:39:34 -0300

    sera

--- Computador/Modulos/CircuitosSequenciais/Ram4K.vhd
+++ Computador/Modulos/CircuitosSequenciais/Ram4K.vhd
@@ -1,16 +1,33 @@
 -- Elementos de Sistemas
 -- by Luciano Soares
--- Ram4K.vhd
+-- Ram8.vhd
 
 Library ieee; 
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
   
-entity Ram4K is
+entity Ram8 is
 	port(
 		clock:   in  STD_LOGIC;
-		input:   in  STD_LOGIC_VECTOR(15 downto 0);
+		input:   in  STD_LOGIC_VECTOR(8191 downto 0);
 		load:    in  STD_LOGIC;
-		address: in  STD_LOGIC_VECTOR(11 downto 0);
-		output:  out STD_LOGIC_VECTOR(15 downto 0)
+		address: in  STD_LOGIC_VECTOR(7 downto 0);
+		output:  out STD_LOGIC_VECTOR(8191 downto 0)
 	);
 end entity;
+
+architecture arch_Ram4k of Ram4k is
+   type memory is array(4095 to 7) of std_logic_vector(8191 downto 0);
+   signal signal_ram : memory;
+
+begin
+
+	process(clock)
+	begin
+		if (clock'event and clock='1' and load='1') then
+				signal_ram(to_integer(unsigned(address))) <= input;
+		end if;
+	end process;
+	output <= signal_ram(to_integer(unsigned(address)));
+
+end architecture;
