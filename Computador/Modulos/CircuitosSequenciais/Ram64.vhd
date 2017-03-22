39d33ceab13e291edc07096ba0c1b9a264ccc927
Filipe Foschini Borba <filipefborba@gmail.com>
2017-03-21 22:26:08 -0300

    ach que vai ram64

--- Computador/Modulos/CircuitosSequenciais/Ram64.vhd
+++ Computador/Modulos/CircuitosSequenciais/Ram64.vhd
@@ -14,3 +14,29 @@ entity Ram64 is
 		output:  out STD_LOGIC_VECTOR(15 downto 0)
 	);
 end entity;
+
+architecture arch_Ram64 of Ram64 is
+
+component Ram8
+	port(
+		clock:   in  STD_LOGIC;
+		input:   in  STD_LOGIC_VECTOR(15 downto 0);
+		load:    in  STD_LOGIC;
+		address: in  STD_LOGIC_VECTOR( 2 downto 0);
+		output:  out STD_LOGIC_VECTOR(15 downto 0)
+	);
+end component;
+
+signal
+
+begin
+	u1: Ram8 port map (clock, input, load, address, w1);
+	u2: Ram8 port map (clock, input, load, address, w2);
+	u3: Ram8 port map (clock, input, load, address, w3);
+	u4: Ram8 port map (clock, input, load, address, w4);
+	u5: Ram8 port map (clock, input, load, address, w5);
+	u6: Ram8 port map (clock, input, load, address, w6);
+	u7: Ram8 port map (clock, input, load, address, w7);
+	u8: Ram8 port map (clock, input, load, address, w8);
+	
+end architecture;
