6077972d7e8ec78c18559ed62b7125f4ddcc91bd
BrunaKimura <brunamk@al.insper.edu.br>
2017-03-23 09:40:24 -0300

    SIPO16 está funcionando!

--- Computador/Modulos/CircuitosSequenciais/ShiftRegisterSIPO16.vhd
+++ Computador/Modulos/CircuitosSequenciais/ShiftRegisterSIPO16.vhd
@@ -20,7 +20,7 @@ architecture arch_ShiftRegisterSIPO16 of ShiftRegisterSIPO16 is
 
 signal w: STD_LOGIC_VECTOR(15 downto 0);
 
-component FlipFlopD
+component FlipFlopD is
 	port(
 			clock:  in std_logic;
 			d:      in std_logic;
@@ -31,22 +31,22 @@ component FlipFlopD
 end component;
 
 begin 
-	u1: FlipFlopD port map (clock, input, '1', '1', w(0));
-	u2: FlipFlopD port map (clock, w(0), '1', '1',  w(1));
-	u3: FlipFlopD port map (clock, w(1), '1', '1',  w(2));
-	u4: FlipFlopD port map (clock, w(2), '1', '1',  w(3));
-	u5: FlipFlopD port map (clock, w(3), '1', '1',  w(4));
-	u6: FlipFlopD port map (clock, w(4), '1', '1',  w(5));
-	u7: FlipFlopD port map (clock, w(5), '1', '1',  w(6));
-	u8: FlipFlopD port map (clock, w(6), '1', '1',  w(7));
-	u9: FlipFlopD port map (clock, w(7), '1', '1',  w(8));
-	u10: FlipFlopD port map (clock, w(8), '1', '1',  w(9));
-	u11: FlipFlopD port map (clock, w(9), '1', '1',  w(10));
-	u12: FlipFlopD port map (clock, w(10), '1', '1',  w(11));
-	u13: FlipFlopD port map (clock, w(11), '1', '1',  w(12));
-	u14: FlipFlopD port map (clock, w(12), '1', '1',  w(13));
-	u15: FlipFlopD port map (clock, w(13), '1', '1',  w(14));
-	u16: FlipFlopD port map (clock, w(14), '1', '1',  w(15));
+	u1: FlipFlopD port map (clock, input, '1', '1', w(15));
+	u2: FlipFlopD port map (clock, w(15), '1', '1',  w(14));
+	u3: FlipFlopD port map (clock, w(14), '1', '1',  w(13));
+	u4: FlipFlopD port map (clock, w(13), '1', '1',  w(12));
+	u5: FlipFlopD port map (clock, w(12), '1', '1',  w(11));
+	u6: FlipFlopD port map (clock, w(11), '1', '1',  w(10));
+	u7: FlipFlopD port map (clock, w(10), '1', '1',  w(9));
+	u8: FlipFlopD port map (clock, w(9), '1', '1',  w(8));
+	u9: FlipFlopD port map (clock, w(8), '1', '1',  w(7));
+	u10: FlipFlopD port map (clock, w(7), '1', '1',  w(6));
+	u11: FlipFlopD port map (clock, w(6), '1', '1',  w(5));
+	u12: FlipFlopD port map (clock, w(5), '1', '1',  w(4));
+	u13: FlipFlopD port map (clock, w(4), '1', '1',  w(3));
+	u14: FlipFlopD port map (clock, w(3), '1', '1',  w(2));
+	u15: FlipFlopD port map (clock, w(2), '1', '1',  w(1));
+	u16: FlipFlopD port map (clock, w(1), '1', '1',  w(0));
 
 	output <= w;
 	
