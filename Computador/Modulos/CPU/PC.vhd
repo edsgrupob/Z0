be32adce2c286360d063f2dc2ca7f5ae03314cf8
Otofuji <ericfoa@al.insper.edu.br>
2017-04-11 15:58:13 -0300

    Importando as portas

--- Computador/Modulos/CPU/PC.vhd
+++ Computador/Modulos/CPU/PC.vhd
@@ -17,3 +17,38 @@ entity PC is
         output    : out STD_LOGIC_VECTOR(14 downto 0)
     );
 end entity;
+
+architecture arch_PC of PC is
+
+component Register16 is
+	port(
+			clock:   in STD_LOGIC;
+			input:   in STD_LOGIC_VECTOR(15 downto 0);
+			load:    in STD_LOGIC;
+			output: out STD_LOGIC_VECTOR(15 downto 0)
+		);
+	end component;
+
+	component Add16 is
+		port(
+			a   :  in STD_LOGIC_VECTOR(15 downto 0);
+			b   :  in STD_LOGIC_VECTOR(15 downto 0);
+			q   : out STD_LOGIC_VECTOR(15 downto 0) 
+		); 
+	end component; 
+
+	component Mux8Way16 is
+
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			b:   in  STD_LOGIC_VECTOR(15 downto 0);
+			c:   in  STD_LOGIC_VECTOR(15 downto 0);
+			d:   in  STD_LOGIC_VECTOR(15 downto 0);
+			e:   in  STD_LOGIC_VECTOR(15 downto 0);
+			f:   in  STD_LOGIC_VECTOR(15 downto 0);
+			g:   in  STD_LOGIC_VECTOR(15 downto 0);
+			h:   in  STD_LOGIC_VECTOR(15 downto 0);
+			sel: in  STD_LOGIC_VECTOR(2 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+end component;
+
