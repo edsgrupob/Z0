1deb76834046722ce4f7dc15f053aff5e5513fe1
Otofuji <ericfoa@al.insper.edu.br>
2017-05-10 11:34:30 -0300

    Começando o esqueleto do VMTranslator

--- Codigos/VMTranslator/src/main/java/vmtranslator/VMTranslator.java
+++ Codigos/VMTranslator/src/main/java/vmtranslator/VMTranslator.java
@@ -5,6 +5,13 @@
 
 package vmtranslator;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import vmtranslator.Parser;
+import vmtranslator.Code;
+
 /**
  * Classe principal que orquestra a tradução do arquivo em linguagem de máquina virtual à pilha.
  * Opções:
@@ -14,8 +21,72 @@ package vmtranslator;
  */
 class VMTranslator {
 
+	private static final String FILENAME = "<arquivo vm>";
+	private static final String OUTPUT = "<arquivo nasm>";
+
     public static void main(String[] args) {
+
+    	Parser parser = new Parser();
+    	Code code = new Code();
+
+    	BufferedReader bufferedReader = null;
+    	FileReader fileReader = null;
+
+    	try {
+    		fileReader = new FileReader(FILENAME);
+    		bufferedReader = new BufferedReader(fileReader);
+
+    		String sCurrentLine;
+
+    		bufferedReader = new BufferedReader(new FileReader(FILENAME));
+
+    		while ((sCurrentLine = br.readLine()) != null) {
+
+
+    			
+
+
+
+
+
+
+
+
+
+
+
+
+    		}
+
+    	}
+
+    	catch (IOException e) {
+    		e.printStackTrace();
+    	}
+
+    	finally {
+    		
+    		try {
+    			
+    			if (bufferedReader != null) {
+    				bufferedReader.close();
+    			}
+    		
+    			if (fileReader != null) {
+    				fileReader.close();
+    			}
+    		}
+
+    		catch (IOException ex) {
+    			ex.printStackTrace();
+    		}
+    	}
 
     }
 
 }
+
+
+
+
+
