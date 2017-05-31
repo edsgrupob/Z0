15da4c9b6482f1687f483595f4ca4318d0d374ea
Filipe Foschini Borba <filipefborba@gmail.com>
2017-05-31 16:21:29 -0300

    Jackcompiler 2.0, i think it's better now

--- Codigos/Compiler/src/main/java/compiler/JackCompiler.java
+++ Codigos/Compiler/src/main/java/compiler/JackCompiler.java
@@ -5,6 +5,8 @@
 
 package compiler;
 
+import java.io.File;
+
 /**
  * Classe principal (main) que orquestra a tradução do arquivo em linguagem de máquina virtual à pilha.
  * Essa classe é responsável por ler os parametros da execução do programa pela linha de código, ou
@@ -20,33 +22,27 @@ class JackCompiler {
 	 * Os parâmetros de linha de comando dever ser tratados nessa rotina.
 	 */ 
 	public static void main(String[] args) {
+		
+		File file = new File(args[0]);
 		String inputName = null;
-    	String outputName = null;
-		int i = 0;
+		String outputName = null;
 
-		if (args.length>0 && args[0].split(".")[1].equals("jack")) {
-			inputName = args[0];
+		boolean exists =      file.exists();      // Check if the file exists
+		boolean isDirectory = file.isDirectory(); // Check if it's a directory
+		boolean isFile =      file.isFile();      // Check if it's a regular file
+		
+		if ((isFile && isDirectory) || exists) {
+			inputName = file.getName().toString();
+			outputName = file.getName().replace(".jack", ".vm").toString();
 		}
 		
-		while(i != args.length){
-
-			if (args[i].equals("-o")){
-				outputName = args[i+1];
-			}
-			
-			i+=1;
-		}
-		
-		if (inputName == null){
+		if (exists && (isFile && isDirectory)){
 			System.err.println("Arquivo de entrada invalido");
 			System.exit(1);
-		}
-
-		if (outputName == null){
-			outputName = (inputName.split(".")[0]+".vm");
 		}
 		
 		CompilationEngine codeVM = new CompilationEngine(inputName, outputName);
 		codeVM.compileClass();
+		//codeVM.close();
 	}
 }
