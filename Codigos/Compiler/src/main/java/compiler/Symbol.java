350d562d6d680f6aa24f22372817010cc22672e4
raphacosta27 <rapha2706@hotmail.com>
2017-05-31 14:57:31 -0300

    Symbol Table maybe ready

--- Codigos/Compiler/src/main/java/compiler/Symbol.java
+++ Codigos/Compiler/src/main/java/compiler/Symbol.java
@@ -9,12 +9,14 @@ package compiler;
  * Classe que organiza os símbolos usados na tabela de símbolos
  */
 public class Symbol {
-
+	private String type;
+	private Kind kind;
+	private Integer index;
     /** Enumerator para os kind. */
     public enum Kind {
         STATIC,
-        FIELD, 
-        ARG, 
+        FIELD,
+        ARG,
         VAR
     }
 
@@ -22,7 +24,18 @@ public class Symbol {
      * Cria um objeto que contem os dados dos símbolos
      */
     public Symbol(String type, Kind kind, Integer index) {
-
+    	this.type = type;
+    	this.kind = kind;
+    	this.index = index;
+    }
+    public String getType() {
+    	return type;
+    }
+    public Kind getKind() {
+    	return kind;
+    }
+    public Integer getIndex() {
+    	return index;
     }
 
 }
